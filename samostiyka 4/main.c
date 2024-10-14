54b590d86b4bd74d1ec03d730a00938b57801222
Kachela <ribnijmaksim@gmail.com>
2024-10-14 09:36:29 +0300

    Добавьте файлы проекта.

---
+++ samostiyka 4/main.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() 
+{
+   
+    float a = -4.53f;
+    float b = 2.74f;
+    float x = -8.07f;
+
+    float result = a * exp(b - x) + cosh(3 * tan(b * x));
+
+    printf("Result: % f\n", result);
+
+    return 0;
+}
